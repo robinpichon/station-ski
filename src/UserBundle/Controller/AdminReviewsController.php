7ad73b542eb507f1f8135a50b927b0b9bd84e4da
Vagrant Default User <vagrant@stretch.localdomain>
2017-10-20 12:51:59 +0000

    Add admin review management

--- src/UserBundle/Controller/AdminReviewsController.php
+++ src/UserBundle/Controller/AdminReviewsController.php
@@ -21,4 +21,61 @@ class AdminReviewsController extends Controller
             'reviews' => $reviews
         ]);
     }
+
+    /**
+     * @Route("/admin/reviews/accept/{id}", name="admin_reviews_accept")
+     */
+    public function acceptAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $review = $em->getRepository(Review::class)->findOneById($id);
+        $review->setStatus(true);
+        $em->persist($review);
+        $em->flush();
+
+        $message = (new \Swift_Message('Votre avis sur '.$review->getStation()->getName().' a été accepté'))
+                ->setFrom(['postmaster@h3r0x.ovh' => 'CriticSki'])
+                ->setTo($this->getUser()->getEmail())
+                ->setBody(
+                    $this->renderView(
+                        'Email/reviewAccepted.html.twig', [
+                          'station' => $review->getStation(),
+                          'user' => $this->getUser(),
+                          'review' => $review
+                        ]
+                    ),
+                    'text/html');
+
+        $this->get('mailer')->send($message);
+        $this->get('session')->getFlashBag()->add('green', 'Commentaire accepté.');
+        return $this->redirectToRoute('admin_reviews');
+    }
+
+    /**
+     * @Route("/admin/reviews/deny/{id}", name="admin_reviews_deny")
+     */
+    public function denyAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $review = $em->getRepository(Review::class)->findOneById($id);
+        $em->remove($review);
+        $em->flush();
+
+        $message = (new \Swift_Message('Votre avis sur '.$review->getStation()->getName().' a été refusé'))
+                ->setFrom(['postmaster@h3r0x.ovh' => 'CriticSki'])
+                ->setTo($this->getUser()->getEmail())
+                ->setBody(
+                    $this->renderView(
+                        'Email/reviewDenied.html.twig', [
+                          'station' => $review->getStation(),
+                          'user' => $this->getUser(),
+                          'review' => $review
+                        ]
+                    ),
+                    'text/html');
+
+        $this->get('mailer')->send($message);
+        $this->get('session')->getFlashBag()->add('green', 'Commentaire refusé.');
+        return $this->redirectToRoute('admin_reviews');
+    }
 }
--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -5,8 +5,11 @@ namespace UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-use SkiBundle\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use UserBundle\Form\AvatarType;
+use SkiBundle\Entity\User;
 
 class DefaultController extends Controller
 {
@@ -15,15 +18,36 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
         $request = Request::createFromGlobals();
-
         $user = $this->getUser();
+
+        $form_account = $this->createFormBuilder()
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+        
         $form = $this->createForm(AvatarType::class, $user);
+
+        $form_account->handleRequest($request);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
+        if($form_account->isSubmitted() && $form_account->isValid()) {
+            $data = $form_account->getData();
 
+            $user->setFirstname($data['firstname'])
+                 ->setLastname($data['lastname'])
+                 ->setEmail($data['email']);
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('green', 'Vos informations ont été mises à jour.');
+        }
+
+        if($form->isSubmitted() && $form->isValid()) {
             /** @var Symfony\Component\HttpFoundation\File\UploadedFile $file */
             $file = $user->getAvatarFile();
             $fileName = md5(uniqid()).'.'.$file->guessExtension();
@@ -51,6 +75,7 @@ class DefaultController extends Controller
         }
 
         return $this->render('UserBundle:Default:index.html.twig', [
+          'form_account' => $form_account->createView(),
           'form' =>  $form->createView()
         ]);
     }
